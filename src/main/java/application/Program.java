a5b013072b644e79e76c979ee81cf8943813ffda
Thales Garcia <thalesgsoares@gmail.com>
2021-11-05 13:51:10 -0300

    Creating a database connection with EntityManager

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,13 +2,24 @@ package application;
 
 import domain.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1, "Thales Garcia", "thales@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Thales Garcia", "thales@gmail.com");  //database will define the id
+        Pessoa p2 = new Pessoa(null, "Roger Souza", "roger@gmail.com");
 
-        System.out.println(p1);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); //same name in persistence.xml (persistence unit)
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(p1); //save the object (p1) in database
+        em.persist(p2); //save the object (p2) in database
+        em.getTransaction().commit();
+        System.out.println("Done!");
 
     }
 
--- src/main/java/domain/Pessoa.java
+++ src/main/java/domain/Pessoa.java
@@ -1,15 +1,24 @@
 package domain;
 
-public class Pessoa {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
-    private int id;
+@Entity
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
     private String name;
     private String email;
 
     public Pessoa(){
     }
 
-    public Pessoa(int id, String name, String email) {
+    public Pessoa(Integer id, String name, String email) {
         this.id = id;
         this.name = name;
         this.email = email;
