5503dfab55de8dc7fec2f7a6ded60df689e94f1c
Thales Garcia <thalesgsoares@gmail.com>
2021-11-05 14:03:42 -0300

    Find an element in database by ID

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,16 +10,12 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(null, "Thales Garcia", "thales@gmail.com");  //database will define the id
-        Pessoa p2 = new Pessoa(null, "Roger Souza", "roger@gmail.com");
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa"); //same name in persistence.xml (persistence unit)
         EntityManager em = emf.createEntityManager();
 
-        em.getTransaction().begin();
-        em.persist(p1); //save the object (p1) in database
-        em.persist(p2); //save the object (p2) in database
-        em.getTransaction().commit();
+        Pessoa p = em.find(Pessoa.class, 2);  //Search the Pessoa with ID=2
+
+        System.out.println(p);
 
         System.out.println("Done!");
 
