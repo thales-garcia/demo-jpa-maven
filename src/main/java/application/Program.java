666e81a2b8557ace6a6e55e383fc828255a3b192
Thales Garcia <thalesgsoares@gmail.com>
2021-11-05 11:59:40 -0300

    First commit

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import domain.Pessoa;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Pessoa p1 = new Pessoa(1, "Thales Garcia", "thales@gmail.com");
+
+        System.out.println(p1);
+
+    }
+
+}
---
+++ src/main/java/domain/Pessoa.java
@@ -0,0 +1,50 @@
+package domain;
+
+public class Pessoa {
+
+    private int id;
+    private String name;
+    private String email;
+
+    public Pessoa(){
+    }
+
+    public Pessoa(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
